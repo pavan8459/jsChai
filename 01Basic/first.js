c95b83b4658108d6628a235c732cab0d26a3b5f9
pavan8459 <155366794+pavan8459@users.noreply.github.com>
2024-11-26 15:35:18 +0000

    let,const,var

--- 01Basic/first.js
+++ 01Basic/first.js
@@ -1,1 +1,14 @@
 console.log("pavankumar")
+
+const accId=12345
+let accEmail="Pavan@gmail.com"
+var accPassword= "332332"
+accCity="Jalgaon"
+
+accEmail="pandit@gmail.com"
+accPassword="2132221"
+accCity="pune"
+
+console.log(accId)
+
+console.table([accId,accEmail,accPassword,accCity])
