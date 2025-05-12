8fafbdfb42f9e87b722a17273061abefd3c479f6
pavan8459 <155366794+pavan8459@users.noreply.github.com>
2025-05-12 10:00:09 +0000

    datatypes

---
+++ 01Basic/datatype.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log(9+8);
+
+/*
+number=> 2^53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined=>
+symbol=> unique
+*/
+
+console.log(typeof undefined);
+console.log(typeof Symbol);
+console.log(typeof null);
+console.log(typeof 8.98);
+
+
+
+
